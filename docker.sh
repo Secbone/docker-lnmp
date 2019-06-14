8d9a119cdcf5a78fd5fccbc60d7fe8d9aeada680
Secbone <secbone@gmail.com>
2019-06-14 22:43:21 +0800

    update: make file

--- docker.sh
+++ docker.sh
@@ -22,6 +22,7 @@ yum install -y docker-ce docker-ce-cli containerd.io
 systemctl start docker
 
 
+# docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
